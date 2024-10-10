d745c68a6b6df0a4de1e6aa31ad854a6c662905d
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-10-10 21:27:12 +0000

    added form, Wikipedia API call, basic error handling

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:form)
+end
+
+get("/test") do
+  title = params.fetch("article_title")
+  guess_age = params.fetch("guess_age").to_i
+  formatted_title = title.gsub(" ", "%20")
+  url = "https://en.wikipedia.org/w/api.php?action=parse&page=#{formatted_title}&format=json"
+  http_response = HTTP.get(url)
+  parsed_response = JSON.parse(http_response)
+  if parsed_response.include?("error")
+    erb(:error_invalid_response)
+  else
+    parse = parsed_response.fetch("parse")
+    text = parse.fetch("text")
+    star_text = text.fetch("*")
+    if star_text.include?("<span class=\"bday\">") == false
+      erb(:error_no_bday)
+    else
+    erb(:test)
+    end
+  end
 end
