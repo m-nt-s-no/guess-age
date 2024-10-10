92fc8a73cb91439cdb6c35ca9ed42f721e8ae932
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-10-10 22:17:49 +0000

    added age regex and comparison logic

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ get("/") do
 end
 
 get("/test") do
-  title = params.fetch("article_title")
-  guess_age = params.fetch("guess_age").to_i
-  formatted_title = title.gsub(" ", "%20")
+  @title = params.fetch("article_title")
+  @guess_age = params.fetch("guess_age").to_i
+  formatted_title = @title.gsub(" ", "%20")
   url = "https://en.wikipedia.org/w/api.php?action=parse&page=#{formatted_title}&format=json"
   http_response = HTTP.get(url)
   parsed_response = JSON.parse(http_response)
@@ -20,10 +20,25 @@ get("/test") do
     parse = parsed_response.fetch("parse")
     text = parse.fetch("text")
     star_text = text.fetch("*")
-    if star_text.include?("<span class=\"bday\">") == false
+    if star_text.include?("<span class=\"noprint ForceAgeToShow\">") == false
       erb(:error_no_bday)
     else
-    erb(:test)
+      age = star_text[/<span class=\"noprint ForceAgeToShow\">(.*)<\/span>/]
+      formatted_age = age.gsub("&#160;", " ")
+      @age_num = formatted_age[/(\d+)/].to_i
+      diff = (@guess_age - @age_num).abs
+      if diff == 0
+        @right_or_wrong = "correct!"
+        @end_text = "Way to go!"
+      else
+        @right_or_wrong = "wrong :("
+      end
+      if (diff > 0) && (diff < 5)
+        @end_text = "You were close though!"
+      elsif diff > 5
+        @end_text = "Not even close lol"
+      end
+      erb(:test)
     end
   end
 end
